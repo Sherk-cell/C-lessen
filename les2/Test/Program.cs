3e4c5bb11943233bf912ee60a1a0e23a07eee237
Sherk-cell <70703520+Sherk-cell@users.noreply.github.com>
2021-11-06 20:33:51 +0100

    3 opdrachten af

---
+++ les2/Test/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace les2
+{
+
+
+
+   
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Enemy healthy = new Enemy();
+            healthy.Attack(23);
+        }
+
+
+        public class Enemy
+        {
+            int Health = 25;
+
+           
+            public void Attack(int damage) {
+                Console.WriteLine("The Enemy's health is " + damage);
+                Console.WriteLine("Would you like to attack? press A");
+                ConsoleKeyInfo info = Console.ReadKey();
+                if (info.KeyChar == 'a')
+                {
+                    Console.Clear();
+                    Console.WriteLine("You Attacked and did "+ damage +" damage");
+                    Health -= damage;
+                    Console.WriteLine("The enemy's health is " + Health);
+                }
+
+
+            }
+            
+            
+                
+
+           
+
+        }
+
+       
+    }
+}
---
+++ les3/les2/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+
+namespace les3
+{
+    class Program
+    {
+        private class Rectangle
+        {
+            private float x;
+            private float y;
+            private float width;
+            private float height;
+
+            public Rectangle(float x, float y, float height, float width)
+            {
+                this.x = x;
+                this.y = y;
+                this.height = height;
+                this.width = width;
+            }
+
+           
+            public Rectangle(float size)
+            {
+                this.width = size;
+                this.height = size;
+            }
+            public Rectangle(float width, float height)
+            {
+                this.width = width;
+                this.height = height;
+            }
+
+            public Rectangle(float x, float y, float size)
+            {
+                this.x = x;
+                this.y = y;
+                this.height = size;
+                this.width = size;
+            }
+
+            public void MoveTo(float x, float y)
+            {
+                this.x = x;
+                this.y = y;
+            }
+
+            public bool IsSquare()
+            {
+                return this.width == this.height;
+            }
+
+            public void Set(float x, float y, float width, float height)
+            {
+                this.x = x;
+                this.y = y;
+                this.width = width;
+                this.height = height;
+            }
+
+            public float GetWidth()
+            {
+                return this.width;
+            }
+
+            public float GetHeight()
+            {
+                return this.height;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Rectangle rechthoek = new Rectangle(100, 100, 50, 50);
+            Rectangle vierkant = new Rectangle(50, 50, 50);
+            Rectangle vierkantBeweeg = new Rectangle(50);
+            Rectangle rechthoekBeweeg = new Rectangle(20, 30);
+
+            vierkantBeweeg.MoveTo(50, 50);
+            rechthoekBeweeg.Set(100, 100, rechthoek.GetWidth(), rechthoek.GetHeight());
+            vierkant.IsSquare();
+
+            Console.WriteLine(vierkant);
+        }
+    }
+}
